92e1e5e08216ed11bcd042c49e2f4b729eaec67a
Tyler Kanz <tylerkanz@gmail.com>
2019-05-22 16:11:34 -0500

    Chapter 4 Commit

---
+++ Chapter 4/simplearithmetic.py
@@ -0,0 +1,20 @@
+number = 1 + 2 * 3 / 4.0
+remainder = 11 % 3
+squared = 7 ** 2
+cubed = 8 ** 3
+helloworld = "hello" + " " + "world"
+lotsofhellos = "hello" * 10
+even_numbers = [2,4,6,8]
+odd_numbers = [1,3,5,7]
+all_numbers = odd_numbers + even_numbers
+
+
+print(number)
+print(remainder)
+print(squared)
+print(cubed)
+print(helloworld)
+print(lotsofhellos)
+print(all_numbers)
+
+print([1,2,3] * 3)
