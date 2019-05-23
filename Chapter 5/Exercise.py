b1a53f7675048ed75ebf542261efa0b56a6d3e5d
Tyler Kanz <tylerkanz@gmail.com>
2019-05-23 14:38:32 -0500

    Chapter 5 & 6

---
+++ Chapter 5/Exercise.py
@@ -0,0 +1,4 @@
+data = ("John", "Doe", 53.44)
+format_string = "Hello %s %s. Your current baalance is $%s."
+
+print(format_string % data)
---
+++ Chapter 5/stringscript.py
@@ -0,0 +1,6 @@
+name = "John"
+age = 23
+mylist = [1,2,3]
+
+print("%s is %d years old." % (name,age))
+print("A list: %s" % mylist)
